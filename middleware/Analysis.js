d15eac4d0bfd3ab9a4fa0276489670ec6d9d0960
HoussemDegachi <houssemdegach@gmail.com>
2025-03-15 21:25:38 +0100

    Finished routes

--- middleware/Analysis.js
+++ middleware/Analysis.js
@@ -1,15 +1,20 @@
-import { getYtMetaData } from "../utils/funcs.js"
+import ExpressError from "../utils/ExpressError.js"
+import { extractYouTubeVideoId, getYtMetaData } from "../utils/funcs.js"
 
 export const isUrlIdValid = async (req, res, next) => {
-    const { id } = req.params
+    const { url } = req.params
+    const id = extractYouTubeVideoId(url)
+
+    if (!url) return next(ExpressError("Url is invalid", 400))
 
     let data;
     try {
         data = await getYtMetaData(id)
     } catch (e) {
-        return next(e)
+        return next(new ExpressError("Video is unavailable", 400))
     }
     req.body.data = data
+    req.body.id = id
     return next()
 }
 
