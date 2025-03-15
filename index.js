ef2a0e95a8cec8b339f9fb29c67d066e6d9ca029
HoussemDegachi <houssemdegach@gmail.com>
2025-03-15 07:39:30 +0100

    Fixed cors calling

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.DB_URL).then(() => console.log("Connected to DB suc
 
 // config
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
