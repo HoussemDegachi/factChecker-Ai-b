dc5e848b531d6f36ee754fc64761d0f3e6f6e22d
HoussemDegachi <113510665+HoussemDegachi@users.noreply.github.com>
2025-03-16 20:21:28 +0100

    added decoding url

--- middleware/Analysis.js
+++ middleware/Analysis.js
@@ -2,7 +2,7 @@ import ExpressError from "../utils/ExpressError.js"
 import { extractYouTubeVideoId, getYtMetaData } from "../utils/funcs.js"
 
 export const isUrlIdValid = async (req, res, next) => {
-    const url = req.params[0]
+    const url = decodeURIComponent(req.params[0])
     const id = extractYouTubeVideoId(url)
 
     if (!url) return next(ExpressError("Url is invalid", 400))
@@ -15,6 +15,7 @@ export const isUrlIdValid = async (req, res, next) => {
     }
     req.body.data = data
     req.body.id = id
+    req.params[0] = url
     return next()
 }
 
