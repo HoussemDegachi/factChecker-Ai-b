794939d55ac1db6245a4c9fa788e2657ee11e436
HoussemDegachi <houssemdegach@gmail.com>
2025-03-17 06:48:42 +0100

    Fixed wrong function calling

--- controller/Analysis.js
+++ controller/Analysis.js
@@ -4,8 +4,7 @@ import ContentAnalysis from "../models/ContentAnalysis.js"
 
 export const get = async (req, res) => {
 
-    const { data, id } = req.body
-    const { url } = req.params
+    const { id } = req.body
 
     const analysis = await ContentAnalysis.findOne({ originalId: id })
 
@@ -13,7 +12,7 @@ export const get = async (req, res) => {
     if (analysis) return res.status(200).json(analysis)
 
     // else create new analysis
-    const analysisResult = await analyzeYoutubeVideo(data.title, url, id);
+    const analysisResult = await analyzeYoutubeVideo(id);
     console.log(analysisResult)
     const newContentAnalysis = new ContentAnalysis(analysisResult)
     await newContentAnalysis.save()
