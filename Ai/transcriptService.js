1f34fce1686aaef6180bf9d1f668a1f42d31a842
HoussemDegachi <houssemdegach@gmail.com>
2025-03-20 14:48:57 +0100

    Added no sandbox mod

--- Ai/transcriptService.js
+++ Ai/transcriptService.js
@@ -9,8 +9,8 @@ puppeteer.use(StealthPlugin())
 
 export async function getYoutubeTranscript(videoId) {
     const browser = await puppeteer.launch({
-        headless: "new",
-        ignoreDefaultArgs: ["--enable-automation", "--no-sandbox", "'--disable-setuid-sandbox'"]
+        headless: "true",
+        ignoreDefaultArgs: ["--no-sandbox", "'--disable-setuid-sandbox'"]
     }); // Starting the headless browser (Chrome)
 
     const page = await browser.newPage();
